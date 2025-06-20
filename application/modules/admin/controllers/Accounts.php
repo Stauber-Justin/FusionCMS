2bdffde682f8ad20784557dc17bf0ac9c79c676f
Justin <124533023+Stauber-Justin@users.noreply.github.com>
2025-06-20 13:51:53 +0200

    Remove mangos checks

--- application/modules/admin/controllers/Accounts.php
+++ application/modules/admin/controllers/Accounts.php
@@ -160,7 +160,7 @@ class Accounts extends MX_Controller
             }
         }
 
-        $external_account_access_data[column("account_access", "gmlevel")] = $this->input->post("gm_level");
+        $external_account_access_data['gmlevel'] = $this->input->post('gm_level');
 
         $internal_account_data["vp"] = $this->input->post("vp");
         $internal_account_data["dp"] = $this->input->post("dp");
