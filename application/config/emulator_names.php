e0d552fa36b5e5847ded4c37a75075c72bffbfc1
Justin <124533023+Stauber-Justin@users.noreply.github.com>
2025-06-20 13:40:06 +0200

    Remove non-Trinity emulator entries

--- application/config/emulator_names.php
+++ application/config/emulator_names.php
@@ -2,9 +2,9 @@
 
 $emulators = [
     'trinity'        => 'TrinityCore (3.3.5a)',
-    'trinity_tww'    => 'TrinityCore - The War Within (11.x.x)',
-    'trinity_df'     => 'TrinityCore - Dragon Flight (10.2.7)',
-    'trinity_sl'     => 'TrinityCore - Shadowlands (9.2.7)',
     'trinity_bfa'    => 'TrinityCore - Battle for Azeroth (8.3.7)',
+    'trinity_df'     => 'TrinityCore - Dragon Flight (10.2.7)',
     'trinity_legion' => 'TrinityCore - Legion (7.3.0)',
+    'trinity_sl'     => 'TrinityCore - Shadowlands (9.2.7)',
+    'trinity_tww'    => 'TrinityCore - The War Within (11.x.x)',
 ];
